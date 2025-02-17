622e7400102e5b14e26abbc8de70cf45fd92ab38
zhaowenxun <zhaowenxun2000@163.com>
2025-02-17 12:57:25 +0800

    fix

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -6,7 +6,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   // 添加标题和描述 borrow from https://pdai.tech/md/about/blog/blog-build-vuepress.html
-  base: '/',
+  base: '/adaxun-doc/',
   locales: {
     "/": {
       lang: "zh-CN",
